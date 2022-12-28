6dd94c0f5d8bc140b3f9875962d2848e8e9923dc
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2022-12-28 21:07:10 +0530

    Time: 107 ms (94.34%), Space: 47.3 MB (31.21%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
+        priority_queue<int, vector<int>, greater<int>> pq;  //USING MIN-HEAP: TC=O(nlogk), SC=O(k)
+        
         for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]);
-        }
-        for(int i=0;i<k-1;i++){
-            pq.pop();
+            if(pq.size()<k){
+                pq.push(nums[i]);
+            }
+            else{
+                if(nums[i]>pq.top()){
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
         }
         return pq.top();
     }
 };
 
-    //Using SORTING:   TC=O(nlgon) , SC=O(1)
+    //1. Using SORTING:   TC=O(nlgon) , SC=O(1)
 
     // sort(nums.begin(),nums.end());   //Ascending order sort
     // for(int i=0;i<nums.size();i++){
@@ -24,3 +30,15 @@ public:
 
     // sort(nums.begin() , nums.end() , greater<int>());  //Descending order sort
     //      return nums[k-1];
+
+    
+    //2. USING MAX-HEAP:    TC=O(nlogn) , SC=O(n)
+    
+    // priority_queue<int> pq; 
+    // for(int i=0;i<nums.size();i++){
+    //     pq.push(nums[i]);
+    // }
+    // for(int i=0;i<k-1;i++){
+    //     pq.pop();
+    // }
+    // return pq.top();
