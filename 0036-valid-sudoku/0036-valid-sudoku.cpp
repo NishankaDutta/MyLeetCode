125c25e77db07f3305dbd92661694875d5dd9de6
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-04 00:09:44 +0530

    Time: 35 ms (67.25%), Space: 20.6 MB (13.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        set<string> s;
+        unordered_map<string, int> m;
                                             //TC=O(k^2) or O(9^2) , SC=O(k^2) or O(9^2)
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
@@ -10,10 +10,10 @@ public:
                     string col = "col"+to_string(j)+board[i][j];
                     string box = "box"+to_string((i/3)*3+(j/3))+board[i][j];
 
-                    if(s.find(row)==s.end() && s.find(col)==s.end() && s.find(box)==s.end()){
-                        s.insert(row);
-                        s.insert(col);
-                        s.insert(box);
+                    if(m[row]!=1 && m[col]!=1 && m[box]!=1){
+                        m[row]=1;
+                        m[col]=1;
+                        m[box]=1;
                     }
                     else{
                         return false;
@@ -24,3 +24,28 @@ public:
         return true;
     }
 };
+
+
+//1. USING SET:    both are same technique only data-structure is different.
+
+//         set<string> s;
+//                                             //TC=O(k^2) or O(9^2) , SC=O(k^2) or O(9^2)
+//         for(int i=0;i<9;i++){
+//             for(int j=0;j<9;j++){
+//                 if(board[i][j]!='.'){
+//                     string row = "row"+to_string(i)+board[i][j];
+//                     string col = "col"+to_string(j)+board[i][j];
+//                     string box = "box"+to_string((i/3)*3+(j/3))+board[i][j];
+
+//                     if(s.find(row)==s.end() && s.find(col)==s.end() && s.find(box)==s.end()){
+//                         s.insert(row);
+//                         s.insert(col);
+//                         s.insert(box);
+//                     }
+//                     else{
+//                         return false;
+//                     }
+//                 }  
+//             }
+//         }
+//         return true;
