5116332dd8dadf1c1655207662b280e15a2cf2b1
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-04 23:18:52 +0530

    Time: 4 ms (37.08%), Space: 8.3 MB (72.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -23,3 +23,47 @@ public:
         
     }
 };
+
+
+// Method 1: Sorting the nums vector
+// Time complexity - O(n log n) , SC=O(1)
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//        sort(nums.begin(), nums.end());
+//     }
+// };
+
+
+// Method 2: keep a count of 0's 1's and 2's
+// Time complexity - O(2N) , SC=O(1)
+
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int count0 = 0;
+//         int count1 = 0; 
+//         int count2  = 0;
+//         for(int i =0; i< nums.size(); i++){
+//             if(nums[i] == 0){
+//                 count0++;
+//             }else if (nums[i] == 1){
+//                 count1++;
+//             }else if (nums[i] ==2 ){
+//                 count2++;
+//             }
+            
+//          }
+//         for(int i =0; i< count0; i++){
+//             nums[i] =0;
+//         }
+//         for(int i =count0; i< (count1 + count0); i++){
+//             nums[i] =1;
+//         }
+//         for(int i =(count1 + count0); i< nums.size(); i++){
+//             nums[i] =2;
+//         }
+        
+//     }
+// };
