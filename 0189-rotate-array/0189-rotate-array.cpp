ecc3b04134de7255f84a5cb310f4c28982eea4d9
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-05 20:17:17 +0530

    Time: 80 ms (28.82%), Space: 25 MB (84.21%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         
-        int n=nums.size();        //TC=O(N) , SC=O(N)  ----> USING EXTRA SPACE
-        vector<int> temp(n);
-        
-        for(int i=0;i<n;i++){
-            temp[(i+k)%n] = nums[i];
-        }
-        nums = temp;        
+        k = k%nums.size();      //if size of given vector is 7 and given k is 10 then, it will be same                                     as 10%7 = 3 rotation
+        reverse(nums.begin() , nums.end());
+        reverse(nums.begin() , nums.begin()+k);
+        reverse(nums.begin()+k , nums.end());
     }
 };
 
-//1. Best method: ----> TC=O(N) , SC=O(1)
+//1. Using extra space: (This method is also good!!!!practice it)
 
- //        k = k%nums.size();      //if size of given vector is 7 and given k is 10 then, it will be same                                     as 10%7 = 3 rotation
- //        reverse(nums.begin() , nums.end());           //TC=O(N) , SC=O(1)
- //        reverse(nums.begin() , nums.begin()+k);
- //        reverse(nums.begin()+k , nums.end());
+//         int n=nums.size();        //TC=O(N) , SC=O(N)
+//         vector<int> temp(n);
+        
+//         for(int i=0;i<n;i++){
+//             temp[(i+k)%n] = nums[i];
+//         }
+//         nums = temp;
