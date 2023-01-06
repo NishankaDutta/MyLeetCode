ebdd2b9e379cf50d5dcbe01023bb7fa175cae248
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-06 18:53:34 +0530

    Time: 180 ms (93.16%), Space: 34.8 MB (16.53%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -9,10 +9,10 @@ public:
         }
         
         for(int i=2;i*i<n;i++){
-            if(!isPrime[i])
-                continue;
-            for(int j=i*i;j<n;j=j+i){
-                isPrime[j] = false;
+            if(isPrime[i]==true){
+                for(int j=i*i;j<n;j=j+i){
+                    isPrime[j] = false;
+                }
             }
         }
         
