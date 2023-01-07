c02a67e80a13ba548765f3d7ef19e8f1c91b3985
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-08 00:24:01 +0530

    Time: 3 ms (98.61%), Space: 13.9 MB (31.53%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxprod = INT_MIN;
+        int maxprod = INT_MIN;          //TC=O(2N)==O(N) , SC=O(1)
         int prod = 1;
         
         for(int i=0;i<nums.size();i++){
