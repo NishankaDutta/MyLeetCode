ce8e5f1e0463a9e15307a45a248a4444c4eee6a4
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-09 00:46:38 +0530

    Time: 46 ms (72.03%), Space: 20.2 MB (17.14%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> ans;
+        if(intervals.size()==0){
+            return ans;
+        }
+        
+        sort(intervals.begin(),intervals.end());      //TC = O(nlogn)+O(n) , SC=O(n)
+        vector<int> tempInterval = intervals[0];
+        
+        for(auto x: intervals){
+            if(tempInterval[1] >= x[0]){
+                tempInterval[1] = max(tempInterval[1],x[1]);
+            } 
+            else{
+                ans.push_back(tempInterval);
+                tempInterval=x;
+            }
+        }
+        ans.push_back(tempInterval);
+        return ans;
+    }
+};
