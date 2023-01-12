528e48511a9068b0a6c38082731cb79807af1d1d
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-12 21:40:04 +0530

    Time: 2408 ms (5.05%), Space: 51.2 MB (23.22%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        set<int> s;
+        int ans = 0;
+        
+        for(int i=0;i<nums.size();i++){            //TC = O(nk) == O(n) , SC = O(n)
+            s.insert(nums[i]);
+        }
+        
+        for(int i=0;i<nums.size();i++){
+            int current_element = nums[i];
+            int previous_element = current_element-1;
+            
+            if(s.find(previous_element)!=s.end())
+                continue;
+            else{
+                int count=0;
+                while(s.find(current_element)!=s.end()){
+                    current_element++;
+                    count++;
+                }
+                ans = max(ans,count);
+            }
+        }
+        return ans;
+    }
+};
