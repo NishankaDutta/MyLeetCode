06a12e8809b40be776ae270feb8f6f7dfb07993e
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-14 15:33:09 +0530

    Time: 30 ms (55.71%), Space: 17.6 MB (64.40%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        vector<int> temp(n);
+        int n=nums.size();
+        priority_queue<int> pq(nums.begin(),nums.end());       //TC = O(N) , SC = O(N)
         
-        int i=1;
-        int j=n-1;
-        
-        while(i<n){
-            temp[i] = nums[j];
-            i=i+2;
-            j--;
+        for(int i=1;i<n;i+=2){
+            nums[i] = pq.top();
+            pq.pop();
         }
         
-        i=0;
-        while(i<n){
-            temp[i] = nums[j];
-            i=i+2;
-            j--;
+        for(int i=0;i<n;i+=2){
+            nums[i] = pq.top();
+            pq.pop();
         }
-        
-        nums=temp;
     }
 };
+
+// int n = nums.size();
+//         sort(nums.begin(),nums.end());         // TC = O(nlogn) , SC=O(n)
+//         vector<int> temp(n);
+        
+//         int i=1;
+//         int j=n-1;
+        
+//         while(i<n){
+//             temp[i] = nums[j];
+//             i=i+2;
+//             j--;
+//         }
+        
+//         i=0;
+//         while(i<n){
+//             temp[i] = nums[j];
+//             i=i+2;
+//             j--;
+//         }
+        
+//         nums=temp;
