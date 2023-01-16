128b4af8cddf18e8217c4474965cff5e7ee9c5a4
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-16 12:40:19 +0530

    Time: 146 ms (70.45%), Space: 109.7 MB (31.76%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;                       //TC = O(n) , SC=O(1)
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] < first)
+                first = nums[i];
+            else if(nums[i] > first && nums[i] < second)
+                second = nums[i];
+            else if(nums[i] > second)
+                return true;
+        }
+        return false;
+    }
+};
+
+
+
+//1. Brute-force approach ------> TC=O(N^2)
+
+// int n = nums.size();
+// for(int i=0;i<n;i++){
+//     for(int j=i+1;j<n;j++){
+//         for(int k=j+1;k<n;k++){
+//             if(nums[i]<nums[j] && nums[j]<nums[k])
+//                 return true;
+//         }
+//     }
+// }
+// return false;
+
