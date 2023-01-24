c1ca607423dabe5206f4e5ce44a6c0c44d4466f9
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-24 20:40:22 +0530

    Time: 11 ms (35.61%), Space: 7.9 MB (10.26%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,49 @@
+class Solution {
+private:
+    bool valid(char ch){
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))
+            return 1;
+        else
+            return 0;
+    }
+    
+    char toLowerCase(char ch){
+        if((ch>='a' && ch<='z') || (ch>='0' && ch<='9'))
+            return ch;
+        else{
+            char temp = ch - 'A' + 'a';
+            return temp;
+        }
+    }
+    
+    bool checkPalindrome(string str){
+        int s = 0;
+        int e = str.length()-1;
+        
+        for(int i=0;i<str.length();i++){
+            if(str[s] != str[e])
+                return false;
+            else{
+                s++;
+                e--;
+            }
+        }
+        return true;
+    }
+
+public:
+    bool isPalindrome(string s) {           //TC = O(N)
+        string temp = "";
+        //Remove all the unnecessary characters except alphabet and numbers 
+        for(int i=0;i<s.length();i++){
+            if(valid(s[i]))
+                temp.push_back(s[i]);
+        }
+        
+        for(int i=0;i<temp.length();i++){
+            temp[i] = toLowerCase(temp[i]);
+        }
+        
+        return checkPalindrome(temp);
+    }
+};
