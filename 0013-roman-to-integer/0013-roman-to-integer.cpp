c3278271737001b038105840609bf510d6b29fbd
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-01-26 16:22:14 +0530

    Time: 31 ms (17.21%), Space: 8.9 MB (9.66%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<char,int> mp{ {'I',1}, {'V',5}, {'X',10}, {'L',50}, {'C',100}, {'D',500}, {'M',1000}};
         
         int value = 0;
-        for(int i=0;i<s.size();i++){               //TC = O(length of the roman string)
+        for(int i=0;i<s.size();i++){               //TC = O(length of the roman string) or O(n)
             if(mp[s[i]] < mp[s[i+1]])
                 value -= mp[s[i]];
             else
