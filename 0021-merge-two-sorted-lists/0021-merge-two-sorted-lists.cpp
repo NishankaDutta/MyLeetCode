15cb0b86fad1d072932a808836d3bca9eac853cd
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-02-06 22:03:04 +0530

    Time: 4 ms (93.26%), Space: 14.9 MB (48.34%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,44 +11,73 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* fh = NULL;
-        ListNode* ft = NULL;          // TC = O(M+N) , SC = O(1)
-        
         if(list1 == NULL)
             return list2;
-        if(list2 == NULL)
+        if(list2 == NULL)              //TC = O(m+n) , SC = O(1) , using dummy node method
             return list1;
         
-        if(list1->val < list2->val){
-            fh = list1;
-            ft = list1;
-            list1 = list1->next;
-        }
-        else{
-            fh = list2;
-            ft = list2;
-            list2 = list2->next;
-        }
+        ListNode* ans = new ListNode();    //Creating a dummy node
+        ListNode* tail = ans;
         
-        while(list1 != NULL && list2 != NULL){
+        while(list1 != NULL && list2 !=NULL){
             if(list1->val < list2->val){
-                ft->next = list1;
-                ft = list1;
+                tail->next = list1;
+                tail = list1;
                 list1 = list1->next;
             }
             else{
-                ft->next = list2;
-                ft = list2;
+                tail->next = list2;
+                tail = list2;
                 list2 = list2->next;
             }
         }
         
-        if(list1 == NULL){
-            ft->next = list2;
-        }
-        else{
-            ft->next = list1;
-        }
-        return fh;
+        if(list1 == NULL)
+            tail->next = list2;
+        else
+            tail->next = list1;
+        
+        return ans->next;
     }
 };
+
+// ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode* fh = NULL;
+//         ListNode* ft = NULL;          // TC = O(M+N) , SC = O(1)
+        
+//         if(list1 == NULL)
+//             return list2;
+//         if(list2 == NULL)
+//             return list1;
+        
+//         if(list1->val < list2->val){
+//             fh = list1;
+//             ft = list1;
+//             list1 = list1->next;
+//         }
+//         else{
+//             fh = list2;
+//             ft = list2;
+//             list2 = list2->next;
+//         }
+        
+//         while(list1 != NULL && list2 != NULL){
+//             if(list1->val < list2->val){
+//                 ft->next = list1;
+//                 ft = list1;
+//                 list1 = list1->next;
+//             }
+//             else{
+//                 ft->next = list2;
+//                 ft = list2;
+//                 list2 = list2->next;
+//             }
+//         }
+        
+//         if(list1 == NULL){
+//             ft->next = list2;
+//         }
+//         else{
+//             ft->next = list1;
+//         }
+//         return fh;
