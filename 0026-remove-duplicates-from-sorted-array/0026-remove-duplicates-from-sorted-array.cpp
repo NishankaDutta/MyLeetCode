b3334dc4fb84aa0a96291a267c1a67988bb59e7a
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-02-16 19:43:16 +0530

    Time: 7 ms (95.41%), Space: 18.5 MB (46.62%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         
         int i=0, j=i+1;              //TC=O(N) , SC=O(1)
         while(j<nums.size()){
-            if(nums[j]==nums[i])
+            if(nums[i]==nums[j])
                 j++;
             else{
                 i++;
