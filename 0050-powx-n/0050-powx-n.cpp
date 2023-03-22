e28a9793dc6d16b81de5ab2f49a200e09d316b74
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-03-22 21:33:53 +0530

    Time: 4 ms (41.47%), Space: 5.9 MB (94.62%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n<0) {
+            n = abs(n);            //TC = O(logn) , SC = O(1)
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else{
+            return x*myPow(x, n-1);
+        }
+    }
+};
