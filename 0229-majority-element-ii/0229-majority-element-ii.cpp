bee7e02efac3fc8b9474e2dcdb9da1f72ae94d6b
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-03-23 21:13:02 +0530

    Time: 11 ms (85.89%), Space: 16 MB (20.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,17 +1,61 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int num1 = INT_MIN, num2 = INT_MIN , count1 = 0, count2 = 0;
         int n = nums.size();
-        unordered_map<int,int> mp;
-        vector<int> v;
-        for(auto x: nums){
-            mp[x]++;
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){        //USING BOYER-MOORE VOTING ALGO. TC = O(N) , SC = O(1)
+            if(nums[i] == num1)
+                count1++;
+            else if(nums[i] == num2)
+                count2++;
+            else if(count1 == 0){
+                num1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                num2 = nums[i];
+                count2 = 1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
         }
         
-        for(auto x: mp){
-            if(x.second > floor(n/3))
-                v.push_back(x.first);
+        count1 = 0, count2 = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==num1)
+                count1++;
+            else if(nums[i]==num2)
+                count2++;
         }
-        return v;
+        
+        if(count1 > n/3)
+            ans.push_back(num1);
+        if(count2 > n/3)
+            ans.push_back(num2);
+        
+        return ans;
     }
 };
+
+
+
+
+
+// Using Unordered map:  TC = O(N) , SC = O(N)
+
+//     int n = nums.size();
+//     unordered_map<int,int> mp;        //TC = O(N) , SC = O(N)
+//     vector<int> v;
+//     for(auto x: nums){
+//         mp[x]++;
+//     }
+    
+//     for(auto x: mp){
+//         if(x.second > floor(n/3))
+//             v.push_back(x.first);
+//     }
+//     return v;
