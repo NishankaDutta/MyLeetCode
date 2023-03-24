5bdb4a940b636db5e5ffa20199da4f8385a66bf6
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-03-24 13:45:15 +0530

    Time: 5 ms (14.16%), Space: 6.6 MB (33.23%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countPaths(int i,int j,int m,int n, vector<vector<int>> &dp){
+        if(i>=m || j>=n)
+            return 0;
+        if(i==m-1 && j==n-1)             //TC = O(M*N) , SC = O(M*N)
+            return 1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        else
+            return dp[i][j] = countPaths(i+1,j,m,n,dp) + countPaths(i,j+1,m,n,dp);
+    }
+    
+    int uniquePaths(int m, int n){
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return countPaths(0,0,m,n,dp);
+    }
+};
+
+
+
+//Using Recursion: (Gives TLE)  ---> TC = O(2^m*n) , SC = O(2^m*n)
+
+// int countPaths(int i, int j, int m, int n){
+//     if(i>=m && j>=n)
+//         return 0;
+//     if(i==m-1 && j==n-1)
+//         return 1;
+//     else
+//         return countPaths(i+1,j,m,n)+countPaths(i,j+1,m,n);
+//     }
+    
+//     int uniquePaths(int m, int n) {
+//         return countPaths(0,0,m,n);
+//     }
