e2724329ea9d5c192cb56d9f27c9f4b2ec69a7c8
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-05-22 14:17:59 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (47.11%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,18 +11,43 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& res){     //TC = O(N), SC = O(1)
-        if(root == NULL)
-            return;
+    vector<int> preorderTraversal(TreeNode* root) {       
+        vector<int> preorder;         //Using Iterative method: TC = O(N), SC = O(N)
+        stack<TreeNode*> st;
         
-        res.push_back(root->val);
-        helper(root->left, res);
-        helper(root->right, res);
-    }
-    
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
-        helper(root,res);
-        return res;
+        if(root == NULL)
+            return preorder;
+        
+        st.push(root);
+        
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            
+            if(root->right != NULL)
+                st.push(root->right);
+            
+            if(root->left != NULL)
+                st.push(root->left);
+        }
+        return preorder;
     }
 };
+
+//1. Using recursion: ->
+
+// void helper(TreeNode* root, vector<int>& res){     //using recursion: TC = O(N), SC = O(1)
+//         if(root == NULL)
+//             return;
+        
+//         res.push_back(root->val);
+//         helper(root->left, res);
+//         helper(root->right, res);
+//     }
+    
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         helper(root,res);
+//         return res;
+//     }
