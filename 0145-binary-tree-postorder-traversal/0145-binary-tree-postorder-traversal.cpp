5bf500e8d394a242bd907ca791b83e35fc515cf1
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-05-23 16:54:58 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (37.15%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,18 +11,48 @@
  */
 class Solution {
 public:
-    void helper(TreeNode* root, vector<int>& res){    //TC = O(N), SC = O(N)   :->Using Recursion
-        if(root == NULL)
-            return;
+    vector<int> postorderTraversal(TreeNode* root) {   
+        vector<int> postorder;          //Iterative way:  TC = O(N) , SC = O(2N)
+        stack<TreeNode*> st1,st2;
         
-        helper(root->left, res);
-        helper(root->right, res);
-        res.push_back(root->val);
-    }
-    
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> res;
-        helper(root, res);
-        return res;
+        if(root == NULL)
+            return postorder;
+        
+        st1.push(root);
+        
+        while(!st1.empty()){
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            
+            if(root->left != NULL)
+                st1.push(root->left);
+            if(root->right != NULL)
+                st1.push(root->right);
+        }
+        
+        while(!st2.empty()){
+            postorder.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return postorder;
     }
 };
+
+
+//1. Using Recursion:
+
+// void helper(TreeNode* root, vector<int>& res){    //TC = O(N), SC = O(N)   :->Using Recursion
+//         if(root == NULL)
+//             return;
+        
+//         helper(root->left, res);
+//         helper(root->right, res);
+//         res.push_back(root->val);
+//     }
+    
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> res;
+//         helper(root, res);
+//         return res;
+//     }
