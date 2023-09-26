2e697e3798f6a6c8de00933ea7128cb18d696174
Nishanka Dutta <99119493+NishankaDutta@users.noreply.github.com>
2023-09-26 22:42:54 +0530

    Time: 11 ms (47.19%), Space: 7.7 MB (16.30%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -21,20 +21,28 @@ public:
         unordered_map<char,int> mp1;
         unordered_map<char,int> mp2;
         
-        for(auto x: s){
-            mp1[x]++;
-        }
+//         for(auto x: s){
+//             mp1[x]++;
+//         }
         
-        for(auto x: t){
-            mp2[x]++;
-        }
+//         for(auto x: t){
+//             mp2[x]++;
+//         }
         
-        for(int i=0;i<s.length();i++){
-            if(mp1[t[i]] != mp2[t[i]])
-                return false;
-        }
+//         for(int i=0;i<s.length();i++){
+//             if(mp1[t[i]] != mp2[t[i]])
+//                 return false;
+//         }
         
-        return true;
+//         return true;
+        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        if(s==t)
+            return true;
+        else
+            return false;
         
         
         
